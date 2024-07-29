85355996b0635c9d8beba566e2c5063c37e56961
= <=>
2024-07-29 16:17:06 +0500

    javascript chapter 1 to 6

---
+++ class 26/app.js
@@ -0,0 +1,67 @@
+// console.log('minhaj');
+// console.log('hello world');
+// consoleLog('hello')
+// console.log('world');
+
+
+// alerts
+// alert("Assalam O Alaikum");
+// ALERT('Hi')
+// window.alert('Hi')
+
+//Pascal case
+// FirstNumber = 89
+
+//lower camel case
+// firstNumber = "minhaj"
+
+//snake case
+// first_number = 89
+
+//kebab case
+// this-is-kebab = 0
+
+
+// firstNumber = "89"
+// alert(firstNumber);
+// console.log(firstNumber)
+
+// variable defining rules
+// 1) number se use nhi kar sakte
+// 1number = 89;
+// 2) special character
+// @#%num = 0
+// 3)space not allowed
+// first number = 9
+
+//decrelization
+var secondNumber;
+
+//initilization
+secondNumber = 12
+secondNumber = 78
+alert(secondNumber)
+
+
+var a  = 20
+var b = 30
+// var c = a add b
+var c = a++
+// console.log(a + b)
+alert(c) 
+var d = a + 2
+console.log(d)
+
+
+var e = 10;   //10
+var f = e++    //10
+var g = ++e + f  +7
+        //11  11  7
+
+//28 shaher uni
+//29 rafay
+// 20 ijaz
+//31  hasseb
+//19   hammad
+// 27 subhan 
+console.log(g)
