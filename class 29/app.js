e0210f0998339274568a10fe331e9e932c9e4813
muhammadminhaj786 <eb20103087.minhajwahid@gmai.com>
2024-08-05 15:50:57 +0500

    class 29

---
+++ class 29/app.js
@@ -0,0 +1,56 @@
+var first_name = '678'
+var x = 2;
+// var inp = +prompt("Enter a value")
+var pass = '123123123'
+
+//typeof
+console.log(typeof(first_name))
+
+// var boolean_var = false
+// console.log(boolean_var)
+
+//if statement
+// if (false) {
+//     alert('chal gaya')
+// }
+
+console.log('line 14')
+
+// if (x === inp){
+//     alert('you are right')
+// }
+
+//==  comparisoin operator   strickly comaprision operator ===
+ console.log(2 == '2')
+ console.log(2 === '2')
+ console.log(2 < 3)
+ console.log(2 >= 2)
+ console.log(5 <= 4.5)
+
+// console.log(0 == false)
+
+// var inp_2 = prompt("Enter your password")
+// //login login
+// if (pass == inp_2) {
+//     alert("login")
+// }
+
+// var city = "KARACHI"
+// var user_city = prompt("Enter your city") 
+// var score = 0
+// if (city == user_city) {
+//     alert('correct')
+//     // score = score + 1
+//     score++
+// }
+// console.log(score)
+
+var age = +prompt("Enter you're age")
+if (age <= 13 ) {
+    alert('you are child')
+}else if(age >13 ) {
+    alert("you're teenager")
+}
+else {
+    alert("you're adult")
+}
