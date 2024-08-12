045f2e6d9ce22bdc88069885b9c01127c6864ca1
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-08-12 15:39:50 +0500

    array start

---
+++ class 32/app.js
@@ -0,0 +1,36 @@
+// var first_name = "farhan"
+// var first_name = "huzaifa"
+// console.log(first_name)
+var first_name ;
+console.log(first_name)
+
+var first_name = ["farhan", "huzaif@", "imran", undefined,]
+                    // 0          1          2   ...
+console.log(first_name.length)                  
+// var farhan = "farhan"
+// console.log(first_name[0] == farhan)
+// first_name[2] = undefined
+// if(first_name[2]==undefined){
+//     alert('true')
+// }else{
+//     alert("false")
+// }
+// console.log(first_name.length)
+
+// console.log(first_name)
+// first_name[1]  = "huzaifa"
+// console.log(first_name)
+// var first_name = ["farhan", "huzaif@", "imran", "bilal" ]
+// var first_name = ["farhan", "huzaif@", "imran","", "", "","","","" ,"bilal" ]
+
+// first_name[10] = "bilal"
+// console.log(first_name)
+
+// var mixed_arr = ["name", 34, true]
+// console.log(first_name[1])
+
+
+// mutable immutable
+var str = "heelo world"
+str = "hello world"
+console.log(str)
