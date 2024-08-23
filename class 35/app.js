014cee6501f701b84c7743324e5ee0af38694472
muhammadminhaj786 <eb20103087.minhajwahid@gmai.com>
2024-08-23 16:02:45 +0500

    loop

---
+++ class 35/app.js
@@ -0,0 +1,42 @@
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+
+// var i;
+// i = 1
+// // i = i + 1
+// // i++
+// console.log(i)
+// for (i=0;i<7;i++) {
+//     console.log('hello')  
+// }
+
+
+// i = 10
+// 10 > 0 true
+// 10
+// 9 > 0 
+// 9
+// 8 > 0 
+// 8 
+// 7 > 0
+// 7
+// 6 > 0 
+// 6
+// 5 > 0
+// 5
+// 1> 0
+// 1
+// 0 > 0 false
+
+// i = 10 
+// 10> 0 true
+// 11 > 0 true
+
+for (var i=10;i>0;i--){
+    console.log(i)
+}
