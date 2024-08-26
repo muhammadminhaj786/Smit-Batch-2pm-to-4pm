45c30c628de3b5b2083b0cea8651e3f5cbf99e80
muhammadminhaj786 <eb20103087minhajwahid@gmail.com>
2024-08-26 15:56:56 +0500

    loop task

---
+++ class 37/app.js
@@ -0,0 +1,54 @@
+// var menu = ["biryani","pulao","bbq","handi","broast", "fish","",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"",,"","rice"]
+// var desired_dish = prompt("Enter dish name");
+// console.log(menu.length)
+
+
+// // "biryani"
+
+// // // console.log(menu[0])
+// // // console.log(menu[1])
+// // // console.log(menu[2])
+// // // console.log(menu[3])
+// // // console.log(menu[4])
+// // // console.log(menu[5])
+
+// var flag = false
+// for (var i =0;i<menu.length;i++){
+//     if(desired_dish == menu[i]){
+//         // "biryani"   == ["biryani","pulao","bbq","handi","broast", "fish"]
+        
+//         flag = true 
+        
+//     }else {
+//         console.log(flag)
+//     }
+// }
+
+// if(flag){
+//     alert("available")
+// }else{
+//     alert("un available")
+// }
+
+// var cleanestCity = ["karachi","lahore","islamabad","multan","quetta"]
+// var myCity = prompt("Enter ur city")
+// var matchFound = false;
+// for (var i =0;i<=4;i++){
+//     if(myCity == cleanestCity[i])
+//         {
+//         alert('aik dafa chala')
+//         matchFound = true
+        
+//     }
+    
+// }
+
+// if(matchFound){
+    
+// }else {
+    
+//     alert("not a good city")
+// }
+
+
+console.log(84 % 2)  
