cdd5bd9409b9fa324fd8179dd69df615c25f6c9e
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-09-04 15:58:21 +0500

    revision

---
+++ class 40/app.js
@@ -0,0 +1,36 @@
+alert("hello world")
+
+var first_name = "pakistan"
+console.log("first_name")
+
+var num = 123
+console.log(num + first_name)
+
+console.log("2",first_name,2+2)
+
+console.log("a" * "b")
+
+var a = 4  //5 //4  //5 //6
+// console.log(a++ + --a + a + a++ + ++a)
+            //   4 + 4   + 4 +  4  + 6
+
+// var b = 3
+// // b = b + 1
+// console.log(++b)
+
+// var my_pass = "123123"
+// var my_gmail = "qwarty@gmail.com"
+// var user_gmail  = prompt("Enter your email");
+// var user_pass = prompt("Enter your password")
+// var age = +prompt("Enter your age")
+
+// if (( age >= 18 || user_gmail == my_gmail) && user_pass == my_pass){
+//     alert("login")
+// }else {
+//     alert("wrong credentials")
+// }
+
+var country = prompt("Enter you country")
+console.log(country.toUpperCase())
+
+
