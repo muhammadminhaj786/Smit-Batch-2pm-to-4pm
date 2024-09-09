e27e49b68281ca720f9a419d4220a091a5e38c70
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-09-09 16:19:19 +0500

    chapter 22

---
+++ class 42/app.js
@@ -0,0 +1,63 @@
+// for (var i =0; i< ;i++){
+//     console.log('hello')
+// }
+// if (){
+//     alert('hello')
+// }else{
+//     alert('hello world')
+// }
+
+// var first_name = 'minhaj'
+
+// // prompt
+// var first_name = prompt('enter your name')
+// // first letter captal
+// var first_capital = first_name[0].toUpperCase()
+// // rest body lower
+// var rest_body = first_name.slice(1,).toLowerCase()
+// // concatenate
+// var result = first_capital + rest_body
+// alert(result)
+
+// var month = prompt("Enter a month");
+// var flag = false
+// var default_months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+// for (var i =0; i<default_months.length; i++){
+//     // console.log(default_months[i])
+//     if(month == default_months[i]){
+//         flag = true
+//         console.log(flag)
+//         break
+//     }else {
+//         flag = false
+//         console.log(flag)
+//     }
+// }
+
+// if(flag){
+//     var month_abbr = month.slice(0, 3);
+// alert(month_abbr);
+// }else {
+//     alert('Invalid month')
+// }
+
+var banned = "world war II";
+console.log(banned.length);
+var sentence =
+  "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien waswriting the trilogy, which contains, with the weird applicability available only to poetry andmyth, the essential notion that the good gray wizard can understand the evil magi preciselybecause he   "
+for (var i =0; i<sentence.length;i++){
+    console.log(i)
+}
