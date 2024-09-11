627049af69c34e7d2608b376ec0005723b54fa9b
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-09-11 15:57:22 +0500

    chapter 23

---
+++ class 43/app.js
@@ -0,0 +1,37 @@
+var para = "Lorem ipsum dolor haseeb sit particular natus pakistan ."
+console.log(para.length)
+// console.log(para.slice(24,))
+
+
+// var first_char = para.indexOf('haseeb')
+// if(first_char !== -1){
+//     para = para.slice(0,first_char ) + 
+// }
+
+// var first_char = para.indexOf('haseeb')
+// console.log(para.slice(0,first_char) +"Irban" + para.slice(24,))
+// console.log(first_char)
+
+var para="Lorem ipsum dolor sit particular natus pakistan sit."
+
+
+var second_Char=para.lastIndexOf("sit")
+console.log(second_Char)
+console.log(para.slice(0,second_Char)+"Huzaifa")
+
+
+var para2 = "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien waswriting the trilogy, which contains, with the weird applicability available only to poetry andmyth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannotgrasp his"
+var third_char = para2.indexOf('World War II')
+console.log(para2.slice(0,third_char) + " the second world war" + para2.slice(73 ,) )
+
+
+var dummy;
+dummy = 123
+console.log(dummy)
+
+
+if (2<2){
+    console.log(true)
+}else {
+    console.log(false)
+}
