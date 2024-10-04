fec80e583a35ccb6dd87801f1f334161a99b8693
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-10-04 15:49:10 +0500

    functions , predefined functions

---
+++ class 52/app.js
@@ -0,0 +1,47 @@
+// var str = 'hello world'
+// console.log(str.indexOf('e')) // find index number
+// console.log(str[4])
+// console.log(str.charAt(4))  // find character
+// console.log(str.length)
+
+// var menu = ['kabab','biryani','daal','handi','fish']
+// var user_req = prompt("Enter a dish ")
+// // var flag = false
+// for (var i=0; i<menu.length;i++){
+//     console.log(menu[i])
+//     if(menu[i] == user_req){
+//        flag = true
+//     }
+// }
+
+// if(flag){
+//     alert('available')
+// }else{
+//     alert('not available')
+// }
+
+// var flag = menu.indexOf(user_req)
+// if(flag != -1){
+//     alert('available')
+// }else{
+//     alert('not available')
+// }
+
+// var user_num = parseInt(prompt("Enter a number"))
+// var sum =0
+// for(var i=1; i<=user_num;i++){
+//     console.log(i)
+//     sum = sum + i
+// }
+// console.log(sum)
+
+// var arr1 = [1,2,12,13,8]
+// var largest = arr1[0]
+// for(var i=0; i<arr1.length;i++){
+//     console.log(arr1[i])
+//     if(arr1[i]> largest){
+//        largest = arr1[i]
+//     }
+// }
+// console.log(largest)
+
