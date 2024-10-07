e6b6e79359e5896d33f2036243ecc15e8005b136
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-10-07 15:45:19 +0500

    switch case

---
+++ class 53/app.js
@@ -0,0 +1,111 @@
+// if(false){
+//     console.log('if')
+// }else{
+//     console.log('else')
+// }
+
+// switch(expressions){
+//     case expressions: 
+//     'case 1'
+//     break
+//     case expressions: 
+//     'case'
+//     break
+//     default: 
+//     'default'
+// }
+
+// var user_inp = prompt("Enter a day ")
+// if(user_inp == 'monday'){
+//     alert('today is monday')
+// }else if(user_inp == 'tuesday'){
+//     alert('today is tuesday')
+// }else if(user_inp == 'wednesday'){
+//     alert('today is wednesday')
+// }else if(user_inp == 'thursday'){
+//     alert('today is thursday')
+// }else if(user_inp == 'Friday'){
+//     alert('today is Friday')
+// }else if(user_inp == 'saturday'){
+//     alert('today is saturday')
+// }else if(user_inp == 'sunday'){
+//     alert('today is sunday')
+// }else{
+//     alert('wrong input')
+// }
+
+//switch case
+// var user_inp = prompt('Enter a day')
+// switch(user_inp){
+//     case 'monday': 
+//         alert('today is monday')
+//     
+//          break
+//     case 'tuesday': 
+//         alert('today is tuesday')
+//     
+//           break
+//     case 'wednesday': 
+//         alert('today is wednesday')
+//     
+//     break
+//     case 'thursday': 
+//         alert('today is thursday')
+//     
+//          break
+//     case 'friday': 
+//         alert('today is friday')
+//     
+//             break
+//     case 'saturday': {
+//         alert('today is saturday')
+//     
+//          break
+//     case 'sunday': 
+//         alert('today is sunday')
+//     
+//          break
+//     default: 
+//         alert('wrong input')
+//     
+// }
+
+// var age = Number(prompt("enter age"))
+
+// console.log(age)
+// if(age > 13 && age<=17 ){
+//     alert("you're a teenager")
+// }else if(age >= 18 ){
+//     alert('adult')
+// }else if (age < 13){
+//     alert('you are a child')
+// }else {
+//     alert('wrong input')
+// }
+
+
+
+// var age = '1'
+// switch (age){
+//     case '1': 
+//         alert('you are a adult')
+//         break
+     
+//     default: 
+//         alert ('wrong input')
+// }
+
+// for(intilization; condition; iteraton){
+    
+// }
+
+//while loop
+// while (condition){
+//     increment
+// }
+
+// var i = 0 
+// while(i<=3){
+//     console.log(i)
+//     i++
+// }
