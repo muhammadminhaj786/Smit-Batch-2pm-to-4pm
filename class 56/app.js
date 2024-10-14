a1eb19cb99b8f72882add0315e8f79ced5d1060b
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-10-14 15:48:29 +0500

    event

---
+++ class 56/app.js
@@ -0,0 +1,20 @@
+// comment
+/*
+multi line comment
+*/
+var newEle = ''
+
+function foo(ele){
+    console.log('button click', ele)
+}
+
+// function submission(name){
+//    alert('hi')
+//    console.log(name)
+// }
+
+
+function checkEmail(id){
+    console.log(id)
+    console.log(document.getElementById(id))
+}
