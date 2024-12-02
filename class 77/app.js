3a65023ac83ae79a13bba2bfc01955bb7bd1601f
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-12-02 15:57:08 +0500

    ES6 start

---
+++ class 77/app.js
@@ -0,0 +1,45 @@
+ "use strict"
+ 
+ firstName = 'minhaj'
+ console.log(firstName)
+
+
+ console.log(lastName)
+ var lastName = 'wahid'
+
+
+ //behind the scene
+// var firstName;
+// var lastName;
+
+//  firstName = 'minhaj'
+//  console.log(firstName)
+//  console.log(lastName)
+
+
+var num = 233
+function showNum(){
+    var newNum = 566
+    console.log(num)
+}
+
+// console.log(newNum)
+newNum = 786
+console.log(newNum)
+showNum()
+
+
+//let const 
+
+// let secondNum = 222
+
+function changeNum(){
+   let secondNum = 111
+    console.log(secondNum)
+}
+
+secondNum = 222
+
+console.log(secondNum)
+
+changeNum()
