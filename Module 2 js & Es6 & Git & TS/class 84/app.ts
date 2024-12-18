dd32bf111a9f4d373a7c9816f4b96f64eef0424d
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-12-18 15:43:27 +0500

    modules

---
+++ Module 2 js & Es6 & Git & TS/class 84/app.ts
@@ -0,0 +1,6 @@
+let num1 = 123
+let num2: number = 123
+let bool = true
+bool = false
+let arr: (number| string)[] = [1,2,34,5]
+arr = [1,'3',5]
---
+++ Module 2 js & Es6 & Git & TS/class 84/math.js
@@ -0,0 +1,12 @@
+export let add = (a,b)=> {
+    console.log(a + b)
+}
+
+export let sub = (a=5,b) => a - b
+
+
+let mul = (a,b)=>{
+    console.log(a*b)
+}
+
+export default mul
