aba6ea96d40086060cad1d44758a87f3804f71c1
muhammadminhaj786 <eb20103087.minhajwahid@gmail>
2025-01-10 15:54:14 +0500

    todos complete with supabase

---
+++ Module 2 js & Es6 & Git & TS/supabase/class 94/app.js
@@ -0,0 +1,99 @@
+
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl = 'https://xfkawxjrcqzuwptcmoya.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhma2F3eGpyY3F6dXdwdGNtb3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxNTUyNzYsImV4cCI6MjA1MTczMTI3Nn0.80an0HWZefSYR8b-eW9zOiEUY8af6dEfFpeg5tkz7X0"
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
+
+
+const input = document.getElementById('inp')
+const ul = document.getElementById('todo-list')
+
+async function createTodo(){
+  
+  const task = input.value
+
+  const { error } = await supabaseClient
+  .from('todos')
+  .insert({  task: task })
+
+  const li = document.createElement('li')
+  li.classList.add('list-item')
+  const span = document.createElement('span')
+  span.innerHTML = task
+  const doneBtn = document.createElement('button')
+  doneBtn.innerHTML = "&#10003;"
+  const delBtn = document.createElement('button')
+  delBtn.innerHTML = "&#10006;"
+
+  li.appendChild(span)
+  li.appendChild(doneBtn)
+  li.appendChild(delBtn)
+
+  ul.appendChild(li)
+
+ 
+}
+
+async function fetchTodos(){
+  const { data, error } = await supabaseClient
+  .from('todos')
+  .select()
+
+  console.log(data)
+
+  for (var i =0; i< data.length; i++){
+    console.log(data[i])
+    const todo = data[i]
+
+    const li = document.createElement('li')
+    li.classList.add('list-item')
+    const span = document.createElement('span')
+    span.innerHTML = data[i].task
+    const doneBtn = document.createElement('button')
+    doneBtn.innerHTML = "&#10003;"
+    doneBtn.addEventListener('click', ()=> {
+      span.classList.add('complete')
+      doneTodos(todo.id, true)
+    })
+    const delBtn = document.createElement('button')
+    delBtn.innerHTML = "&#10006;"
+    delBtn.addEventListener('click', ()=> {
+      delTodos(todo.id)
+      console.log(todo.id)
+    })
+  
+    li.appendChild(span)
+    li.appendChild(doneBtn)
+    li.appendChild(delBtn)
+  
+    ul.appendChild(li)
+
+  }
+
+
+
+}
+
+
+async function delTodos(id){
+  const response = await supabaseClient
+  .from('todos')
+  .delete()
+  .eq('id', id)
+
+  console.log('delete')
+  window.location.reload()
+
+
+}
+
+async function doneTodos(id, is_Done){
+  const { error } = await supabaseClient
+  .from('todos')
+  .update({ is_Done: is_Done})
+  .eq('id', id)
+
+}
+
+fetchTodos()
