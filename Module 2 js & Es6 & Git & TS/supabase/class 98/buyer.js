4d57ea293dabeafe287363a2d6b40942a58ea71e
muhammadminhaj786 <=>
2025-01-20 16:09:52 +0500

    buyer

---
+++ Module 2 js & Es6 & Git & TS/supabase/class 98/buyer.js
@@ -0,0 +1,108 @@
+const supabaseUrl = "https://iuwrpxqtiuklkyngdpod.supabase.co";
+
+
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
+const parentDiv = document.querySelector('.my-cards')
+
+async function fetchProducts(){
+    const { data, error } = await supabaseClient
+    .from('products')
+    .select()
+
+    console.log(data)
+
+    for(var i=0; i<data.length;i++){
+        console.log(data[i])
+        const product = data[i]
+
+        // // create a parent div
+        // const cardDiv = document.createElement('div')
+        // cardDiv.classList.add('card')
+        // cardDiv.style.width = "18rem";
+
+        // // create a image
+        // const img = document.createElement('img')
+        // img.classList.add('card-img-top')
+        // img.setAttribute('src', `images/${product.image}`)
+
+        // //card body
+        // const cardBody = document.createElement('div')
+        // cardBody.classList.add('card-body')
+
+        // //create h5
+        // const h5 = document.createElement('h5')
+        // h5.classList.add('card-title')
+        // h5.innerHTML = product.title
+
+        // //create description
+        // const p = document.createElement('p')
+        // p.classList.add('card-text')
+        // p.innerHTML = product.description
+
+        // //create price
+        // const priceP = document.createElement('p')
+        // priceP.innerHTML = `${product.price} PKR`
+
+        // //create button div
+        // const btnDiv = document.createElement("div")
+        // const buyBtn = document.createElement('button')
+        // // buyBtn.classList.add("btn", "btn-success")
+        // buyBtn.className = "btn btn-success"
+        // buyBtn.innerHTML = 'Buy Now'
+
+        // const cartBtn = document.createElement('button')
+        // cartBtn.className = "btn btn-info"
+        // cartBtn.innerHTML = 'Add to cart'
+
+        // cardDiv.appendChild(img)
+        // btnDiv.appendChild(cartBtn)
+        // btnDiv.appendChild(buyBtn)
+        // cardBody.appendChild(btnDiv)
+
+        // cardBody.appendChild(priceP)
+
+        // cardBody.appendChild(p)
+
+        // cardBody.appendChild(h5)
+        // cardDiv.appendChild(cardBody)
+
+
+        
+        // parentDiv.appendChild(cardDiv)
+
+        const cardDiv = document.createElement('div')
+        cardDiv.innerHTML = `
+         <div class="card" style="width: 18rem;">
+            <img src="images/${product.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text">${product.description}.</p>
+              
+                <p>
+                    ${product.price} <span>PKR</span>
+                </p>
+
+               <div>
+                <button class="btn btn-success">
+                    Buy Now
+                </button>
+                <button class="btn btn-info">
+                    Add To Cart
+                </button>
+               </div>
+
+            </div>
+        </div>
+        `
+
+        parentDiv.appendChild(cardDiv)
+
+
+    }
+   
+
+
+
+}
+
+fetchProducts()
---
+++ Module 2 js & Es6 & Git & TS/supabase/class 98/vendor.js
@@ -0,0 +1,35 @@
+console.log('vendor')
+
+const supabaseUrl = "https://iuwrpxqtiuklkyngdpod.supabase.co";
+
+
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
+
+const titleInp = document.getElementById('title')
+const descInp = document.getElementById('desc')
+const priceInp = document.getElementById('price')
+
+async function addProduct(){
+    title = titleInp.value
+    desc = descInp.value
+    price = priceInp.value
+    console.log(title, price, desc)
+
+    const { error } = await supabaseClient
+  .from('products')
+  .insert([
+    {
+        title: title,
+        description: desc,
+        price: price
+    }
+  ])
+
+  if(!error){
+    alert('product created successfully')
+  }
+
+  console.log(error)
+
+
+}
