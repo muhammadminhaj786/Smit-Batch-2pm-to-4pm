84986a086a3fad434335c52a961f14f7d17bae2b
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-02-14 15:31:20 +0500

    state

---
+++ Module 3 React/class 04/ui/src/component/card/Card.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './Card.css'
+
+export function Card(){
+
+    const price = 5400
+
+
+    const [name, setName] = useState('minhaj')
+
+    let [count, setCount] = useState(0)
+
+
+    
+
+    function increment(){
+        
+        setCount(++count)
+        console.log('chala', count)
+    }
+
+    function updateName (){
+        setName('abc')
+    }
+
+    return (
+        <div className="card">
+            <div>
+                <img className='img' src="src\assets\airpods.jpg_.webp" alt="" />
+            </div>
+            <div className='ctn'>
+                <p>
+                Airpods Air Pro 3rd Gen TWS (True Wireless Stereo)
+                </p>
+               <div className='divCtn'>
+               <p className='price'>
+                    Rs {price}
+                </p>
+                <p>
+                    view <span>{count}</span>
+                </p>
+               </div>
+                <div className='btnDiv'>
+                <button onClick={increment} className='btn'>
+                    Buy Now
+                </button>
+                </div>
+            </div>
+        </div>
+    )
+}
