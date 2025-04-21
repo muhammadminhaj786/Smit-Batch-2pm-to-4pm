d06693e9166d7ffdd2964c22eed7b211d540b114
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-04-21 16:06:41 +0500

    home

---
+++ Module 3 React/class 27/e-commerce/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MyNavBar from '../component/MyNavBar'
+import MyCrousel from '../component/MyCrousel'
+import Banner from '../component/Banner'
+
+
+const Home = () => {
+  return (
+<div>
+<MyNavBar />
+<Banner />
+</div>
+  )
+}
+
+export default Home
